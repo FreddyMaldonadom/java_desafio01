c14787b1b55418b10fe2713abaeb6e108d2c968b
Freddy Maldonado <contacto@freddymaldonado.com>
2024-04-26 16:38:28 -0400

    Actualización

--- src/TRABAJOS/EJERCICIOEVALUADOPATRONES/PatronesAnidados.java
+++ src/TRABAJOS/EJERCICIOEVALUADOPATRONES/PatronesAnidados.java
@@ -7,35 +7,138 @@ public class PatronesAnidados {
 
 //PATRON 01
 
-        System.out.print(" Escribe un número del 1 al 5");
+//        System.out.print(" Escribe un número del 1 al 5");
+//        int n = sc.nextInt();
+//
+//        if (n == 1) {
+//            System.out.println("*");
+//        } else if (n == 2) {
+//            System.out.println("**");
+//        } else if (n == 3) {
+//            System.out.println("***");
+//            System.out.println("* *");
+//            System.out.println("***");
+//        } else if (n == 4) {
+//            System.out.println("****");
+//            System.out.println("*  *");
+//            System.out.println("*  *");
+//            System.out.println("****");
+//        } else if (n == 5) {
+//            System.out.println("*****");
+//            System.out.println("*   *");
+//            System.out.println("*   *");
+//            System.out.println("*   *");
+//            System.out.println("*****");
+//        } else {
+//            System.out.println("Mal número. Ingresa un número del 1 al 5.");
+//           }
+//       }
+//}
+
+//PATRON 02
+//
+//        System.out.print(" Escribe un número del 1 al 5");
+//        int n = sc.nextInt();
+//
+//        if (n == 1) {
+//            System.out.println("*");
+//        } else if (n == 2) {
+//            System.out.println("**");
+//        } else if (n == 3) {
+//            System.out.println("**");
+//            System.out.println(" * ");
+//            System.out.println("**");
+//        } else if (n == 4) {
+//            System.out.println("****");
+//            System.out.println("  * ");
+//            System.out.println(" *  ");
+//            System.out.println("****");
+//        } else if (n == 5) {
+//            System.out.println("*****");
+//            System.out.println("   * ");
+//            System.out.println("  *  ");
+//            System.out.println(" *   ");
+//            System.out.println("*****");
+//        } else {
+//            System.out.println("Mal número. Ingresa un número del 1 al 5.");
+//        }
+//    }
+//}
+
+    //PATRON 03
+//
+//        System.out.print(" Escribe un número del 1 al 6");
+//        int n = sc.nextInt();
+//
+//        if (n == 1) {
+//            System.out.println("X");
+//        } else if (n == 2) {
+//            System.out.println("xx");
+//            System.out.println("xx");
+//        } else if (n == 3) {
+//            System.out.println("x x");
+//            System.out.println(" x ");
+//            System.out.println("x x");
+//        } else if (n == 4) {
+//            System.out.println("x  x");
+//            System.out.println(" xx ");
+//            System.out.println(" xx  ");
+//            System.out.println("x  x");
+//        } else if (n == 5) {
+//            System.out.println("x   x");
+//            System.out.println(" x x ");
+//            System.out.println("  x  ");
+//            System.out.println(" x x ");
+//            System.out.println("x   x");
+//        } else if (n == 6) {
+//            System.out.println("x    x");
+//            System.out.println(" x  x ");
+//            System.out.println("  xx  ");
+//            System.out.println("  xx  ");
+//            System.out.println(" x  x ");
+//            System.out.println("x    x");
+//        } else {
+//            System.out.println("Mal número. Ingresa un número del 1 al 6.");
+//        }
+//    }
+//}
+
+    //PATRON 04
+
+        System.out.print(" Escribe un número del 1 al 6");
         int n = sc.nextInt();
 
         if (n == 1) {
             System.out.println("*");
         } else if (n == 2) {
-            System.out.println("**");
+            System.out.println("* ");
+            System.out.println(" *");
         } else if (n == 3) {
-            System.out.println("***");
-            System.out.println("* *");
-            System.out.println("***");
+            System.out.println("** ");
+            System.out.println(" * ");
+            System.out.println(" **");
         } else if (n == 4) {
-            System.out.println("****");
-            System.out.println("*  *");
-            System.out.println("*  *");
-            System.out.println("****");
+            System.out.println("*** ");
+            System.out.println(" ** ");
+            System.out.println(" ** ");
+            System.out.println(" ***");
         } else if (n == 5) {
+            System.out.println("****");
+            System.out.println(" ***");
+            System.out.println(" ***");
+            System.out.println(" ***");
+            System.out.println(" ****");
+        } else if (n == 6) {
             System.out.println("*****");
-            System.out.println("*   *");
-            System.out.println("*   *");
-            System.out.println("*   *");
-            System.out.println("*****");
+            System.out.println(" ****");
+            System.out.println(" ****");
+            System.out.println(" ****");
+            System.out.println(" ****");
+            System.out.println(" *****");
         } else {
-            System.out.println("Mal número. Ingresa un número del 1 al 5.");
-           }
-       }
+            System.out.println("Mal número. Ingresa un número del 1 al 6.");
+        }
+    }
 }
-
-//PATRON 02
-
 
 
